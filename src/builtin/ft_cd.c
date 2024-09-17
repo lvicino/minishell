df87a0bbc470261fa3db46229425a51a63c511aa
Ruben Gallien <rgallien@paul-f5Ar3s9.clusters.42paris.fr>
2024-09-17 17:52:56 +0200

    fix a little cd, check more bug

--- src/builtin/ft_cd.c
+++ src/builtin/ft_cd.c
@@ -6,11 +6,12 @@
 /*   By: rgallien <rgallien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 15:01:46 by lvicino           #+#    #+#             */
-/*   Updated: 2024/09/17 09:41:15 by rgallien         ###   ########.fr       */
+/*   Updated: 2024/09/17 17:34:17 by rgallien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
 
 char	*return_allocate(void)
 {
@@ -21,6 +22,7 @@ char	*return_allocate(void)
 		return (NULL);
 	return (pwd);
 }
+
 
 void	replace_pwd(char *n_pwd, char *o_pwd, t_env **env)
 {
@@ -46,6 +48,33 @@ void	replace_pwd(char *n_pwd, char *o_pwd, t_env **env)
 	}
 }
 
+int	ft_cd_without_args(t_env *env)
+{
+	char		*o_pwd;
+	char		*n_pwd;
+	struct stat	path_stat;
+
+	while (env && ft_strncmp(env->var, "HOME", bigger(env->var, "HOME")))
+		env = env->next;
+	if (!env)
+		return (ft_putstr_fd("bash: cd: HOME not set\n", 1), 1);
+	o_pwd = return_allocate();
+	n_pwd = return_allocate();
+	getcwd(o_pwd, 4096);
+	if (!stat(env->value, &path_stat))
+	{
+		if (!S_ISDIR(path_stat.st_mode))
+			return (ft_putstr_fd(env->value, 2), \
+			ft_putstr_fd(": not a directory\n", 2), 1);
+		else
+			return (chdir(env->value), getcwd(n_pwd, 4096), \
+			replace_pwd(n_pwd, o_pwd, &env), 0);
+	}
+	else
+		return (perror(env->value), 1);
+	return (1);
+}
+
 int	ft_cd(t_env **env, char **cmd, int cmd_ln, int fd)
 {
 	char		*o_pwd;
@@ -55,25 +84,21 @@ int	ft_cd(t_env **env, char **cmd, int cmd_ln, int fd)
 	(void)fd;
 	if (cmd_ln > 2)
 		return (ft_putstr_fd("Minishell : cd: too many arguments\n", 2), 1);
+	else if (cmd_ln == 1)
+		 return (ft_cd_without_args(*env));
 	o_pwd = return_allocate();
 	n_pwd = return_allocate();
 	getcwd(o_pwd, 4096);
-	if (cmd_ln > 0)
+	if (!stat(cmd[1], &path_stat))
 	{
-		if (!stat(cmd[1], &path_stat))
-		{
-			if (!S_ISDIR(path_stat.st_mode))
-				return (ft_putstr_fd(cmd[1], 2), \
-				ft_putstr_fd(": not a directory\n", 2), 1);
-			else
-				return (printf("return\n"), chdir(cmd[1]), getcwd(n_pwd, 4096), \
-				replace_pwd(n_pwd, o_pwd, env), 0);
-		}
+		if (!S_ISDIR(path_stat.st_mode))
+			return (ft_putstr_fd(cmd[1], 2), \
+			ft_putstr_fd(": not a directory\n", 2), 1);
 		else
-		{
-			printf("else\n");
-			return (perror(cmd[0]), 1);
-		}
+			return (chdir(cmd[1]), getcwd(n_pwd, 4096), \
+			replace_pwd(n_pwd, o_pwd, env), 0);
 	}
+	else
+		return (perror(cmd[0]), 1);
 	return (1);
 }
